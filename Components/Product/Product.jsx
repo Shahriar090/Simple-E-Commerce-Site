1d8f807f62b2936cfbfdebb2533a2d28278cf2a2
Shahriar090 <shahriarh655@gmail.com>
2023-10-21 11:58:30 +0600

    Added product information and applied some css styles to the product cards

--- Components/Product/Product.jsx
+++ Components/Product/Product.jsx
@@ -3,14 +3,17 @@ import './Product.css'
 
 const Product = ({product}) => {
     console.log(product)
-    const {img, seller, quantity, price, name} = product;
+    const {img, seller, quantity, price, name,ratings} = product;
     return (
         <div className='product-container'>
             <img src={img} alt="ProductImg" />
-            <h3>Product Name : {name}</h3>
+           <div className="product-info">
+           <h3 className='product-name'>Product Name : {name}</h3>
             <h3>Manufacturer : {seller}</h3>
             <h4>Quantity : {quantity}</h4>
-            <h4>Price : {price}</h4>
+            <h4>Price : ${price}</h4>
+            <h4>Ratings : {ratings}/5</h4>
+           </div>
             <button className='btn-add-cart'>Add To Cart</button>
             
         </div>
